a9c2d7f062e6129e587b0c85766c20b05eec16c4
remi.chaoul <remi.chaoul@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-10 21:15:46 +0000

    unmessage

---
+++ Semaphore/FeuBicolor.java
@@ -0,0 +1,24 @@
+
+public class FeuBicolor extends Semaphore {
+
+	protected boolean Feu1;
+	protected boolean Feu2;
+	
+	public Semaphore () {
+
+		this.Feu1=false;
+		this.Feu2=true;
+			
+	    };
+
+	    
+	    boolean isGreen(){
+	    	return this.Feu1;
+	    }
+	    
+	    void toGreen(){
+	    	this.Feu1=true;
+	    	this.Feu2=false;
+	    }
+	    
+}
