01b421c78989c1c58218b53f171d6fe45c9d7846
meneo.tk <meneo.tk@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-10 21:04:52 +0000

    Add element de regulation

---
+++ trunk/Regulation/ElementDeRegulation.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Regulation;
+
+/**
+ *
+ * @author po
+ */
+public class ElementDeRegulation {
+    private ArrayList<Aiguillage> aguillages;
+    private ArrayList<Capteur> capteurs;
+    private ArrayList<Semaphore> semaphores;
+}
