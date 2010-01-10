c6f3ac11c73246272408d5392de87b6f404a9629
jeanbaptiste.richardet <jeanbaptiste.richardet@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-10 20:51:37 +0000

    All class Elementsdevoie

---
+++ trunk/ElementsDeVoie/Jonction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ElementsDeVoie;
+
+/**
+ *
+ * @author NorTicUs
+ */
+public class Jonction {
+
+    private Rail amont;
+    private Rail aval;
+}
---
+++ trunk/ElementsDeVoie/Rail.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ElementsDeVoie;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author NorTicUs
+ */
+public class Rail {
+
+    private ArrayList<Troncon> troncons;
+    private ElementsJonction amont;
+    private ElementsJonction aval;
+    private ArrayList<Semaphore> semaphoresAmont;
+    private ArrayList<Semaphore> semaphoresAval;
+}
