b874bced40bd1252239060f43f62c99d8a08b948
jeanbaptiste.richardet <jeanbaptiste.richardet@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-10 21:11:55 +0000

    Get & Set !

--- ElementsDeVoie/Aiguillage.java
+++ ElementsDeVoie/Aiguillage.java
@@ -5,14 +5,36 @@
 
 package ElementsDeVoie;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  *
  * @author NorTicUs
  */
-public class Aiguillage {
+public class Aiguillage extends ElementsJonction{
 
-    private ArrayList<Rail> amont;
-    private ArrayList<Rail> aval;
+    private LinkedList<Rail> amont;
+    private LinkedList<Rail> aval;
+
+    public Aiguillage(LinkedList<Rail> amont, LinkedList<Rail> aval) {
+	this.amont = amont;
+	this.aval = aval;
+    }
+
+    public LinkedList<Rail> getAmont() {
+	return amont;
+    }
+
+    public void setAmont(LinkedList<Rail> amont) {
+	this.amont = amont;
+    }
+
+    public LinkedList<Rail> getAval() {
+	return aval;
+    }
+
+    public void setAval(LinkedList<Rail> aval) {
+	this.aval = aval;
+    }
+
 }
--- ElementsDeVoie/Butee.java
+++ ElementsDeVoie/Butee.java
@@ -9,7 +9,20 @@ package ElementsDeVoie;
  *
  * @author NorTicUs
  */
-public class Butee {
+public class Butee extends ElementsJonction{
 
     private Rail rail;
+
+    public Butee(Rail rail) {
+	this.rail = rail;
+    }
+
+    public Rail getRail() {
+	return rail;
+    }
+
+    public void setRail(Rail rail) {
+	this.rail = rail;
+    }
+
 }
