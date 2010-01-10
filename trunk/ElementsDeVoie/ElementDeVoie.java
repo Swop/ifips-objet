6f15c020cb099bbaf83cb2dbc6ce63609dd12184
jeanbaptiste.richardet <jeanbaptiste.richardet@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-10 18:52:28 +0000

    class Abstract ElementDeVoie

---
+++ trunk/ElementsDeVoie/ElementDeVoie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ElementsDeVoie;
+
+/**
+ *
+ * @author NorTicUs
+ */
+public abstract class ElementDeVoie {
+
+}
