f1eae220ac47a27b687f717c49a0609a9b5d34ee
meneo.tk <meneo.tk@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-10 21:47:10 +0000

    Ajout capteur

---
+++ Capteur/CapteurPosition.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Capteur;
+
+/**
+ *
+ * @author po
+ */
+public class CapteurPosition extends Capteur{
+
+}
