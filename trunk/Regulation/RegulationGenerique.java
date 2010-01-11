763dd9873fac4b7a21a024fc201e35d540a90f7f
meneo.tk <meneo.tk@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-11 05:47:15 +0000

    regulation générique w00t

--- trunk/Regulation/RegulationGenerique.java
+++ trunk/Regulation/RegulationGenerique.java
@@ -21,16 +21,32 @@ public class RegulationGenerique extends ElementDeRegulation {
 
     Random ia;
     public void reguler() {
+
         for (Aiguillage a : this.aguillages){
             try {
                 a.setLien(ia.nextInt(a.getAmont().size()), ia.nextInt(a.getAval().size()));
+
             } catch (TrainSurAiguillage ex) {
                 return;
             }
-            for (Semaphore s : a.getLienAmont().getSemaphoresAmont()) {
+            System.out.println("Echange des liens et tentative de mise à jour feu");
+            for (Semaphore s : a.getLienAmont().getSemaphoresAval()) {
                 ((FeuUnicolor)s).setFeu1(false);
-                for (Rail ram : a.getAmont())
-                    for (Semaphore sam : ram.getSemaphoresAval())
+                this.addSemaphore(s);
+            }
+            for (Semaphore s : a.getLienAval().getSemaphoresAmont()) {
+                ((FeuUnicolor)s).setFeu1(false);
+                this.addSemaphore(s);
+            }
+            for (Rail ram : a.getAmont()){
+                for (Semaphore sam : ram.getSemaphoresAval())
+                    if (!this.getSemaphore().contains(sam))
+                        ((FeuUnicolor)sam).setFeu1(true);
+
+            }
+            for (Rail ram : a.getAval()){
+                for (Semaphore sam : ram.getSemaphoresAmont())
+                    if (!this.getSemaphore().contains(sam))
                         ((FeuUnicolor)sam).setFeu1(true);
             }
         }
