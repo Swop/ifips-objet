b7d3c9b71196ceeecd71ffba36bf527dccab4c80
remi.chaoul <remi.chaoul@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-11 00:44:06 +0000

    t

--- Semaphore/FeuUnicolor.java
+++ Semaphore/FeuUnicolor.java
@@ -24,27 +24,41 @@ public class FeuUnicolor extends Semaphore {
 	 */
 	protected boolean Feu1;
 	
+	/**
+	 * constructeur de FeuUnicolor
+	 * @param sens sens du FeuUnicolor	
+	 * @param position position du FeuUnicolor
+	 */
 	public FeuUnicolor (Sens sens,Troncon position) {
 		super(sens,position);
 		this.Feu1=false;//Feu Vert
 			
 	    };
 
-	    
-	    boolean getFeu1(){
+	    /**
+	     * retourne la valeur de Feu 1
+	     * @return valeur (vrai ou faux) de Feu1
+	     */
+	    public boolean getFeu1(){
 	    	return Feu1;
 	    }
 	    
-	    void setFeu1(boolean bool){
+	    /**
+	     * modifie la valeur du Feu1
+	     * @param bool nouvelle valeur du Feu1
+	     */
+	    public void setFeu1(boolean bool){
 	    	this.Feu1=bool;
 	    }
 
 
 		/**
-		 * Modifie la vitesse du train
+		 * Modifie la vitesse du train en fonction de la valeur de Feu1
 		 */
 		public void actionTrain(Train t) {
-			//if(!Feu1)
+			if(!Feu1)t.setVitessePourcentage(0);
+			else if(t.getVitesseCourante()==0) t.setVitessePourcentage((float) 0.50);
+			
 		}
 	    
 }
