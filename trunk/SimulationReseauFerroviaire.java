c858da2361409d18f1cee2cc3a842c863bea328a
nano.swop <nano.swop@871e0eb6-fd5d-11de-b136-7f7962f7bc17>
2010-01-11 03:07:11 +0000

    Init réseau

--- trunk/SimulationReseauFerroviaire.java
+++ trunk/SimulationReseauFerroviaire.java
@@ -26,30 +26,30 @@ public class SimulationReseauFerroviaire {
     private static void initReseau() {
 	// init
 
-	Rail r1 = new Rail();
-	Rail r2 = new Rail();
+	Rail r1 = new Rail(5);
+	Rail r2 = new Rail(5);
 	r2.addSemaphoreAmont(new ChangementVitesse());
 
 	Jonction j1 = new Jonction();
 	j1.setAmont(r2);
 	j1.setAval(r1);
 
-	Aiguillage a1 = new Aguillage();
+	Aiguillage a1 = new Aiguillage();
 
-	Rail r3 = new Rail();
+	Rail r3 = new Rail(5);
 	a1.addAval(r3);
 	a1.addAval(r2);
 
-	Rail r4 = new Rail();
-	Rail r5 = new Rail();
+	Rail r4 = new Rail(5);
+	Rail r5 = new Rail(5);
 	a1.addAmont(r4);
 	a1.addAmont(r5);
 	
 	Aiguillage a2 = new Aiguillage();
 	a2.addAval(r3);
 	
-	Rail r6 = new Rail();
-	Rail r7 = new Rail();
+	Rail r6 = new Rail(5);
+	Rail r7 = new Rail(5);
 	r7.addSemaphoreAval(new FeuTricolor());
 	a2.addAmont(r6);
 	a2.addAmont(r7);
@@ -57,7 +57,7 @@ public class SimulationReseauFerroviaire {
 	Jonction j2 = new Jonction();
 	j2.setAval(r7);
 	
-	Rail r8 = Rail();
+	Rail r8 = new Rail(5);
 	r8.addSemaphoreAmont(new PanneauRalentissement());
 	j2.setAmont(r8);
 	
@@ -66,13 +66,13 @@ public class SimulationReseauFerroviaire {
 	a3.addAval(r6);
 	a3.addAval(r5);
 	
-	Rail r9 = new Rail();
+	Rail r9 = new Rail(5);
 	Butee b = new Butee();
 	b.setRail(r9);
 
 	a3.addAmont(r9);
 
-	Rail r10 = new Rail();
+	Rail r10 = new Rail(5);
 	r10.addSemaphoreAmont(new FeuTricolore());
 	a3.addAmont(r10);
 	
